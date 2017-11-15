f47ce6ea8388f298c5c684b60b4b8f365ab05165
Sam Atkins <samatkins5@gmail.com>
2017-11-15 14:23:41 +0000

    tests: Add new action test, and fix App.test.js

---
+++ client/src/actions/categoryActions.test.js
@@ -0,0 +1,67 @@
+/* global afterEach, describe, expect, it */
+
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import fetchMock from 'fetch-mock';
+import {
+  fetchCategories,
+  RECEIVE_CATEGORIES,
+} from './categoryActions';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const mockedAPIReturn = {
+  categories: [
+    {
+      name: 'react',
+      path: 'react',
+    },
+    {
+      name: 'redux',
+      path: 'redux',
+    },
+    {
+      name: 'udacity',
+      path: 'udacity',
+    },
+  ],
+};
+
+describe('categoryActions', () => {
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
+  it('should execute fetch data', () => {
+    fetchMock.getOnce('http://localhost:3001/categories', mockedAPIReturn);
+
+    const expectedActions = [
+      {
+        categories: [
+          {
+            name: 'react',
+            path: 'react',
+          },
+          {
+            name: 'redux',
+            path: 'redux',
+          },
+          {
+            name: 'udacity',
+            path: 'udacity',
+          },
+        ],
+        type: RECEIVE_CATEGORIES,
+      },
+    ];
+
+    const store = mockStore({ mockedAPIReturn });
+
+    return store.dispatch(fetchCategories()).then(() => {
+      const actions = store.getActions();
+      expect(actions).toEqual(expectedActions);
+    });
+  });
+});
