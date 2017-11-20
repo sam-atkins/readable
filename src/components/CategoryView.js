6a70459976c4d91fe39e9e2b82daaf6a51fc6419
Sam Atkins <samatkins5@gmail.com>
2017-11-20 16:19:42 +0000

    feat: Add select menu for posts in a category

--- src/components/CategoryView.js
+++ src/components/CategoryView.js
@@ -7,8 +7,13 @@ const CategoryView = props => (
   <ViewWrapper>
     <CategoryContent>
       <CategoryHeader>{props.category.name}</CategoryHeader>
-      <PostView />
+      <SortPostsMenu>
+        <option disabled>Sort posts by:</option>
+        <option>Sort by vote</option>
+        <option>Sort by comment</option>
+      </SortPostsMenu>
     </CategoryContent>
+    <PostView />
   </ViewWrapper>
 );
 
@@ -22,7 +27,15 @@ const CategoryContent = styled.div`
 `;
 
 const CategoryHeader = styled.h2`
+  grid-column-start: 1;
+  grid-row-start: 1;
   text-decoration: underline;
+`;
+
+const SortPostsMenu = styled.select`
+  grid-column-start: 1;
+  grid-row-start: 1;
+  cursor: pointer;
 `;
 
 CategoryView.propTypes = {
