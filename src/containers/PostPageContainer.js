d03e2beca6b2768a9ca70b9b8a6e0f373b6a7d88
Sam Atkins <samatkins5@gmail.com>
2017-11-23 21:08:35 +0000

    feat: Render filtered post
    [WIP]

--- src/containers/PostPageContainer.js
+++ src/containers/PostPageContainer.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import NavBarContainer from './NavBarContainer';
 import Header from '../components/Header';
+import PostView from '../components/PostView';
 import Footer from '../components/Footer';
+import { selectPostByPostId, validPostUrl } from '../selectors/postSelectors';
 
 import PageWrapper from '../styles/pagewrapper';
 
-const PostPageContainer = ({ match }) => (
+const PostPageContainer = ({ selectedPost, validPostUrlId, match }) => (
   <StyledWrapper>
     <Header />
     <NavBarContainer />
-    <div>This is a page with one post. Id: {match.params.postId} Slug: {match.params.postSlug}</div>
+    {selectedPost.map(post => <PostView post={post} />)}
     <Footer />
   </StyledWrapper>
 );
 
 const StyledWrapper = styled(PageWrapper)``;
 
-export default PostPageContainer;
+const mapStateToProps = (state, ownProps) => ({
+  selectedPost: selectPostByPostId(state, ownProps.match.params.postId),
+  validPostUrlId: validPostUrl(state, ownProps.match.params.postId),
+});
+
+export default connect(mapStateToProps)(PostPageContainer);
