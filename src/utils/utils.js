e41f747e6396b3e9cd166a6630821c26bffa8257
Sam Atkins <samatkins5@gmail.com>
2017-12-05 10:25:29 +0000

    feat: Add util function for form field validators

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,5 +5,20 @@ export const slugifyPostTitle = postTitle =>
   slugify(postTitle, { lower: true, remove: /[$*_+~.()'"!\-:@]/g });
 
 const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+export const createRandomID = number => _.sampleSize(char, number).join('');
 
-export const createRandomID = number => (_.sampleSize(char, number).join(''));
+export const userInputIsValid = (fieldInput, num, type) => {
+  if (type === 'greater') {
+    if (fieldInput.length > num) {
+      return true;
+    }
+  }
+
+  if (type === 'less') {
+    if (fieldInput.length < num) {
+      return true;
+    }
+  }
+
+  return false;
+};
--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,6 +1,6 @@
 /* global describe, it, expect */
 
-import { slugifyPostTitle } from './utils';
+import { slugifyPostTitle, userInputIsValid } from './utils';
 
 describe('slugifies a post title', () => {
   it('should lower case a capitalised post title', () => {
@@ -25,5 +25,42 @@ describe('slugifies a post title', () => {
     const postTitle = 'I ♥ Redux';
     const slugUrl = 'i-love-redux';
     expect(slugifyPostTitle(postTitle)).toBe(slugUrl);
+  });
+});
+
+describe('validates user input', () => {
+  it('should return true if string is greater than 0', () => {
+    const fieldInput = 'a';
+    const num = 0;
+    const type = 'greater';
+    expect(userInputIsValid(fieldInput, num, type)).toBeTruthy();
+  });
+
+  it('should return true if string is greater than 0', () => {
+    const fieldInput = 'hello';
+    const num = 0;
+    const type = 'greater';
+    expect(userInputIsValid(fieldInput, num, type)).toBeTruthy();
+  });
+
+  it('should return false if string is empty', () => {
+    const fieldInput = '';
+    const num = 0;
+    const type = 'greater';
+    expect(userInputIsValid(fieldInput, num, type)).toBeFalsy();
+  });
+
+  it('should return true if string is less than 12', () => {
+    const fieldInput = 'hello world';
+    const num = 12;
+    const type = 'less';
+    expect(userInputIsValid(fieldInput, num, type)).toBeTruthy();
+  });
+
+  it('should return false if string is greater than 10', () => {
+    const fieldInput = 'hello world';
+    const num = 10;
+    const type = 'less';
+    expect(userInputIsValid(fieldInput, num, type)).toBeFalsy();
   });
 });
