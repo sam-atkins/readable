805c9c77107437877ff02ce04cac46ee459ed22c
Sam Atkins <samatkins5@gmail.com>
2017-12-08 22:32:24 +0000

    style: LInt file

--- src/components/NoMatchWrapper.js
+++ src/components/NoMatchWrapper.js
@@ -6,7 +6,6 @@ import Footer from './Footer';
 import NoMatchText from './NoMatchText';
 import PageWrapper from '../styles/pagewrapper';
 
-
 const NoMatchWrapper = () => (
   <StyledWrapper>
     <Header />
