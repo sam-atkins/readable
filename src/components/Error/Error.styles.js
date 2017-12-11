6eb335138d93f4b99ad47d826c5eb920d953ae2a
Sam Atkins <samatkins5@gmail.com>
2017-12-11 21:01:42 +0000

    refactor: Reorg component to folder structure

---
+++ src/components/Error/Error.styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { TEXT_WARNING } from '../../styles/colours';
+
+export const ErrorMessageWrapper = styled.div`
+  text-align: center;
+`;
+
+export const ErrorText = styled.p`
+  color: ${TEXT_WARNING};
+`;
