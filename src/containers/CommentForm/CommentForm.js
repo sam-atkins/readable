781094c758bb2436a8ab212c225ae0cfc86090d6
Sam Atkins <samatkins5@gmail.com>
2017-12-12 19:04:21 +0000

    feat: add basic structure, header etc

--- src/containers/CommentForm/CommentForm.js
+++ src/containers/CommentForm/CommentForm.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import NavbarContainer from '../NavBarContainer';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import { StyledWrapper } from './CommentForm.styles';
 
 class CommentForm extends Component {
   state = {};
   render() {
-    return <div>CommentForm</div>;
+    return (
+      <fragment>
+        <StyledWrapper>
+          <Header />
+          <NavbarContainer />
+          <div>Comment form</div>
+          <Footer />
+        </StyledWrapper>
+      </fragment>
+    );
   }
 }
 
--- src/containers/CommentForm/CommentForm.styles.js
+++ src/containers/CommentForm/CommentForm.styles.js
@@ -1,1 +1,4 @@
 import styled from 'styled-components';
+import PageWrapper from '../../styles/pagewrapper';
+
+export const StyledWrapper = styled(PageWrapper) ``;
