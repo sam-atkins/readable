061f074dffbdda65fd2eca2a427b35b35c6c1eb8
Sam Atkins <samatkins5@gmail.com>
2017-12-14 14:56:15 +0000

    feat: actions and reducers to add  comments

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -1,4 +1,4 @@
-import { fetchCommentsForSinglePost } from '../utils/api';
+import { fetchCommentsForSinglePost, persistComment } from '../utils/api';
 
 export const RECEIVE_COMMENTS_SUCCESS = 'RECEIVE_COMMENTS_SUCCESS';
 export const receiveCommentsSuccess = payload => ({
@@ -15,3 +15,20 @@ export const fetchComments = payload => dispatch =>
   fetchCommentsForSinglePost(payload)
     .then(comments => dispatch(receiveCommentsSuccess(comments)))
     .catch(error => dispatch(receiveCommentsFail(error)));
+
+export const ADD_NEW_COMMENT_SUCCESS = 'ADD_NEW_COMMENT_SUCCESS';
+export const addCommentSuccess = payload => ({
+  type: ADD_NEW_COMMENT_SUCCESS,
+  payload,
+});
+
+export const ADD_NEW_COMMENT_FAILURE = 'ADD_NEW_COMMENT_FAILURE';
+export const addCommentError = () => ({
+  type: ADD_NEW_COMMENT_FAILURE,
+});
+
+export const addCommentPost = payload => (dispatch) => {
+  persistComment(payload)
+    .then(data => dispatch(addCommentSuccess(data)))
+    .catch(error => dispatch(addCommentError(error)));
+};
