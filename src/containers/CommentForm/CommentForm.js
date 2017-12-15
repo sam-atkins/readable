17f94b0a915baa016e21b10a238c0b32bdacd56d
Sam Atkins <samatkins5@gmail.com>
2017-12-15 12:42:48 +0000

    feat: Add comments via form

--- src/containers/CommentForm/CommentForm.js
+++ src/containers/CommentForm/CommentForm.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import Button from '../../components/Button';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addCommentPost } from '../../actions/commentActions';
 import {
   Buffer,
   FormWrapperLabel,
+  StyledButton,
   StyledForm,
   StyledLabel,
   StyledTextArea,
@@ -10,8 +13,33 @@ import {
 } from './CommentForm.styles';
 
 class CommentForm extends Component {
-  state = {};
+  state = {
+    // parentId field is required for requests to add/edit a comment
+    /* eslint-disable */
+    parentId: this.props.parentId,
+    /* eslint-enable */
+    body: this.props.body,
+    author: this.props.author,
+  };
+
   render() {
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+
+      this.setState({
+        [name]: value,
+      });
+    };
+
+    const handleFormSubmit = (event) => {
+      event.preventDefault();
+      this.props.submitFormToAddComment(this.state);
+      this.setState({
+        body: '',
+        author: '',
+      });
+    };
+
     return (
       <StyledForm>
         <Buffer />
@@ -19,20 +47,49 @@ class CommentForm extends Component {
           <StyledLabel>Comment</StyledLabel>
         </FormWrapperLabel>
         <FormWrapperLabel>
-          <StyledTextArea cols="20" rows="8" />
+          <StyledTextArea
+            value={this.state.body}
+            name="body"
+            cols="20"
+            rows="8"
+            onChange={event => handleInputChange(event)}
+          />
         </FormWrapperLabel>
-
         <FormWrapperLabel>
           <StyledLabel>Username</StyledLabel>
         </FormWrapperLabel>
         <FormWrapperLabel>
-          <StyledInput />
+          <StyledInput
+            name="author"
+            value={this.state.author}
+            onChange={event => handleInputChange(event)}
+          />
         </FormWrapperLabel>
         <Buffer />
-        <Button />
+        <StyledButton onClick={event => handleFormSubmit(event)}>
+          save
+        </StyledButton>
       </StyledForm>
     );
   }
 }
 
-export default CommentForm;
+CommentForm.propTypes = {
+  body: PropTypes.string,
+  author: PropTypes.string,
+  parentId: PropTypes.string.isRequired,
+  submitFormToAddComment: PropTypes.func.isRequired,
+};
+
+CommentForm.defaultProps = {
+  body: '',
+  author: '',
+};
+
+const mapDispatchToProps = dispatch => ({
+  submitFormToAddComment: (payload) => {
+    dispatch(addCommentPost(payload));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(CommentForm);
--- src/containers/CommentForm/CommentForm.styles.js
+++ src/containers/CommentForm/CommentForm.styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import {
+  COMMENT_BUTTON_BACKGROUND,
+  COMMENT_BUTTON_TEXT,
   FORM_BUFFER_BACKGROUND,
   FORM_WRAPPER_LABEL_BACKGROUND,
 } from '../../styles/colours';
@@ -56,4 +58,19 @@ export const StyledInput = styled.input`
 export const StyledSelect = styled.select`
   padding: 0 10px 0 10px;
   margin-left: 10px;
+`;
+
+export const StyledButton = styled.div`
+  background: linear-gradient(${COMMENT_BUTTON_BACKGROUND});
+  border-radius: 4px;
+  color: ${COMMENT_BUTTON_TEXT};
+  cursor: pointer;
+  font-size: 14px;
+  line-height: 2.16;
+  text-align: center;
+  white-space: nowrap;
+  border: 0px;
+  padding: 0 1.066667em;
+  min-width: 50px;
+  width: 60px;
 `;
