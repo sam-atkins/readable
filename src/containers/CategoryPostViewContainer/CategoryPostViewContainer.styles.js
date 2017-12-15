31d6dda5e4d6646f3d66d5fef524d03c9ded7474
Sam Atkins <samatkins5@gmail.com>
2017-12-15 20:58:01 +0000

    refactor: Change to folder structure

---
+++ src/containers/CategoryPostViewContainer/CategoryPostViewContainer.styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const CategoryViewWrapper = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 4;
+  grid-row: 2;
+`;
+
+export const CategoryPostWrapper = styled.div``;
+
+export const CategoryContent = styled.div`
+  margin-left: 1rem;
+`;
+
+export const CategoryHeader = styled.h2`
+  grid-column-start: 1;
+  grid-row-start: 1;
+  text-decoration: underline;
+`;
+
+export const SortPostsMenu = styled.select`
+  grid-column-start: 1;
+  grid-row-start: 1;
+  cursor: pointer;
+`;
