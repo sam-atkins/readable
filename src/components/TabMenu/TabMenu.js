4bb54b9c7bebbc0956029e6194004bd5b67fb3d1
Sam Atkins <samatkins5@gmail.com>
2017-12-26 17:39:57 +0000

    feat: Add new sort posts by old and low

--- src/components/TabMenu/TabMenu.js
+++ src/components/TabMenu/TabMenu.js
@@ -4,29 +4,46 @@ import PropTypes from 'prop-types';
 import { Wrapper, TabMenuList, TabMenuItem } from './TabMenu.styles';
 import {
   userRequestSortByHighestVote,
+  userRequestSortByLowestVote,
   userRequestSortByNew,
+  userRequestSortByOld,
 } from '../../actions/postActions';
 
-const TabMenu = ({ sortByHighestVotes, sortByNewest }) => (
+const TabMenu = ({
+  sortByHighestVotes,
+  sortByLowestVotes,
+  sortByNewest,
+  sortByOldest,
+}) => (
   <Wrapper>
     <TabMenuList>
       <TabMenuItem onClick={() => sortByNewest()}>New</TabMenuItem>
+      <TabMenuItem onClick={() => sortByOldest()}>Old</TabMenuItem>
       <TabMenuItem onClick={() => sortByHighestVotes()}>Top</TabMenuItem>
+      <TabMenuItem onClick={() => sortByLowestVotes()}>Low</TabMenuItem>
     </TabMenuList>
   </Wrapper>
 );
 
 TabMenu.propTypes = {
   sortByHighestVotes: PropTypes.func.isRequired,
+  sortByLowestVotes: PropTypes.func.isRequired,
   sortByNewest: PropTypes.func.isRequired,
+  sortByOldest: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = dispatch => ({
   sortByNewest: () => {
     dispatch(userRequestSortByNew());
   },
+  sortByOldest: () => {
+    dispatch(userRequestSortByOld());
+  },
   sortByHighestVotes: () => {
     dispatch(userRequestSortByHighestVote());
+  },
+  sortByLowestVotes: () => {
+    dispatch(userRequestSortByLowestVote());
   },
 });
 
